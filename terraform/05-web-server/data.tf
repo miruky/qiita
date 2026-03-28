f6ffb6f11cb0db22b96ab254ab4d167c5f4367ec
miruky <miruky@users.noreply.github.com>
2026-03-28 17:08:53 +0900

    feat(terraform): 【Terraform #5】データソース(AMI/VPC)

---
+++ terraform/05-web-server/data.tf
@@ -0,0 +1,23 @@
+# data.tf
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
