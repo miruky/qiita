9048c4636d576c217a20e2bf727e73ab92d0cb86
miruky <miruky@users.noreply.github.com>
2026-03-28 17:08:48 +0900

    feat(terraform): 【Terraform #3】dynamicブロック

---
+++ terraform/03-hcl-basics/dynamic_block.tf
@@ -0,0 +1,52 @@
+variable "ingress_rules" {
+  type = list(object({
+    port        = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+  default = [
+    {
+      port        = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "HTTP"
+    },
+    {
+      port        = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "HTTPS"
+    },
+    {
+      port        = 22
+      protocol    = "tcp"
+      cidr_blocks = ["10.0.0.0/8"]
+      description = "SSH from internal"
+    },
+  ]
+}
+
+resource "aws_security_group" "web" {
+  name        = "web-sg"
+  description = "Web server security group"
+  vpc_id      = aws_vpc.main.id
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      description = ingress.value.description
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
