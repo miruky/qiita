8186ddcab811df7d0aaf0f716eddbbcbf96f81d4
miruky <miruky@users.noreply.github.com>
2026-03-28 17:08:48 +0900

    feat(terraform): 【Terraform #2】プロバイダー設定

---
+++ terraform/02-first-deploy/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.14.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project   = "terraform-first-project"
+      ManagedBy = "Terraform"
+    }
+  }
+}
